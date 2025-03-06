3b914ec49e2e753dbc4c7da648a156e15a861e1f
Rhea-Anne Danao <158535192+rheaanne@users.noreply.github.com>
2025-03-06 06:34:02 +0000

    fix link

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -4,8 +4,7 @@ import About from "@/components/About.vue";
 import Reference from "@/components/Reference.vue";
 import Contact from "@/components/Contact.vue";
 
-const routes = [
-  { path: "/", redirect: "/index" }, 
+const routes = [ 
   { path: "/index", component: Index },
   { path: "/about", component: About },
   { path: "/reference", component: Reference },
